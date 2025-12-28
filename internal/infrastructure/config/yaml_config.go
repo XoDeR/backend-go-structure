1967e1bdaa6489708ee6889fcdae03267208b064
XoDeR <xoder.vs@gmail.com>
2025-12-28 15:57:17 +0100

    added database section to config

--- internal/infrastructure/config/yaml_config.go
+++ internal/infrastructure/config/yaml_config.go
@@ -9,8 +9,9 @@ import (
 )
 
 type AppConfig struct {
-	App    AppSection    `yaml:"app"`
-	Server ServerSection `yaml:"server"`
+	App      AppSection      `yaml:"app"`
+	Server   ServerSection   `yaml:"server"`
+	Database DatabaseSection `yaml:"database"`
 }
 
 type AppSection struct {
@@ -27,6 +28,19 @@ type ServerSection struct {
 	ReadTimeout     time.Duration `yaml:"read_timeout"`
 	WriteTimeout    time.Duration `yaml:"write_timeout"`
 	ShutdownTimeout time.Duration `yaml:"shutdown_timeout"`
+}
+
+type DatabaseSection struct {
+	Host            string        `yaml:"host"`
+	Port            int           `yaml:"port"`
+	User            string        `yaml:"user"`
+	Password        string        `yaml:"password"`
+	Database        string        `yaml:"database"`
+	SSLMode         string        `yaml:"sslmode"`
+	MaxOpenConns    int           `yaml:"max_open_conns"`
+	MaxIdleConns    int           `yaml:"max_idle_conns"`
+	ConnMaxLifetime time.Duration `yaml:"conn_max_lifetime"`
+	ConnMaxIdleTime time.Duration `yaml:"conn_max_idle_time"`
 }
 
 func Load() (*AppConfig, error) {
